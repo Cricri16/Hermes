97f4f2c744de88795cd3fcccef5b8afc317a3ce8
Cricri16 <129993793+Cricri16@users.noreply.github.com>
2025-02-10 13:47:45 +0100

    Update update.py

--- update.py
+++ update.py
@@ -2,15 +2,16 @@ import urllib.request
 import os
 from tkinter import messagebox
 import requests
+local = 'v14.6.3'
 def chekupdate():
     url = "https://raw.githubusercontent.com/Cricri16/Hermes/main/version.txt"
     with urllib.request.urlopen(url) as f:
         fich = f.readlines()
-        version = fich[0][:-1].decode('utf-8')
-    with open('version.txt','r') as f:
-        local = f.readlines()[0][:-1]
+        version = fich[0][:-1].decode('utf-8')[:-1]
     if version != local:
-        messagebox.showinfo('instalation','la messagerie Hermes va etre installer')
+        print(version+']')
+        exit()
+        messagebox.showinfo('mise a jou','la messagerie Hermes va etre mise a jour')
         nombre_de_fichier = int(fich[1][:-1].decode('utf-8')) # on recupere le nombre de fichier a telecharger
         liste_a_telecharger = [] # on creer une liste qui va contenir les fichier a telecharger
         for i in range(nombre_de_fichier): # on fait une boucle pour recuperer les fichier a telecharger
@@ -21,4 +22,4 @@ def chekupdate():
             with urllib.request.urlopen(url) as f:
                 with open(i[:-1],'wb') as file:
                     file.write(f.read())
-        messagebox.showinfo('instalation','instalation effectuer')
+        messagebox.showinfo('mise a jour','mise a jour effectuer')
